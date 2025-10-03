c029a150036de93044b82401b99b16630fe92c4d
beans <sendit2isaiah@gmail.com>
2025-10-02 21:51:53 -0700

    Added missing import from firebaseMultiplayer

--- src/services/firebaseMultiplayer.js
+++ src/services/firebaseMultiplayer.js
@@ -1,10 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set, get, onValue, remove, update } from "firebase/database";
 import { firebaseConfig } from '../config/firebase.js';
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 export class MultiplayerService {
   constructor() {
