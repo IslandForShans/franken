e3b7c2fd1de93613b3616606760c6988305228f9
beans <sendit2isaiah@gmail.com>
2025-10-02 21:35:36 -0700

    deleted unused useRef

--- src/components/DraftSimulator.jsx
+++ src/components/DraftSimulator.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Sidebar.jsx";
 import FactionSheet from "./FactionSheet.jsx";
 import DraftHistory from "./DraftHistory.jsx";
